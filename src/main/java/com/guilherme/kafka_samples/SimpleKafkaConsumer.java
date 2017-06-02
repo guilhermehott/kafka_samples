9b0fa3b63bd0aef54909c4aac0debba871b40206
Guilherme Hott <guilherme@clout.com>
2017-06-02 15:29:45 -0700

    refactoring and add log4j

--- src/main/java/com/guilherme/kafka_samples/SimpleKafkaConsumer.java
+++ src/main/java/com/guilherme/kafka_samples/SimpleKafkaConsumer.java
@@ -1,46 +1,64 @@
 package com.guilherme.kafka_samples;
 
-import java.util.Properties;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author ghott
+ *
+ */
 public class SimpleKafkaConsumer {
-   public static void main(String[] args) throws Exception {
-//      if(args.length == 0){
-//         System.out.println("Enter topic name");
-//         return;
-//      }
-      //Kafka consumer configuration settings
-      String topicName = "TutorialClout"; //args[0].toString();
-      Properties props = new Properties();
-      
-      props.put("bootstrap.servers", "localhost:9092");
-      props.put("group.id", "test");
-      props.put("enable.auto.commit", "true");
-      props.put("auto.commit.interval.ms", "1000");
-      props.put("session.timeout.ms", "30000");
-      props.put("key.deserializer", 
-         "org.apache.kafka.common.serialization.StringDeserializer");
-      props.put("value.deserializer", 
-         "org.apache.kafka.common.serialization.StringDeserializer");
-      KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-      
-      //Kafka Consumer subscribes list of topics here.
-      consumer.subscribe(Arrays.asList(topicName));
-      
-      //print the topic name
-      System.out.println("Subscribed to topic " + topicName);
-      int i = 0;
-      
-      while (true) {
-         ConsumerRecords<String, String> records = consumer.poll(100);
-         for (ConsumerRecord<String, String> record : records)
-         
-         // print the offset,key and value for the consumer records.
-         System.out.printf("offset = %d, key = %s, value = %s\n", 
-            record.offset(), record.key(), record.value());
-      }
-   }
+
+	private static final Logger LOG = LoggerFactory.getLogger(SimpleKafkaConsumer.class);
+	private static KafkaConsumer<String, String> consumer;
+
+	public static void main(String[] args) throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+		String origin = "origin.";
+
+		// Kafka consumer configuration settings
+		List<String> topics = Arrays.asList(origin+"guilhermehott",origin+"Topic2",origin+"PlaidWebhook",origin+"at-least-once",origin+"exactly-once",origin+"exactly-once-test");
+
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "dev-kafka.clout.com:9092");
+		props.put("group.id", "test-consumer-group");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("session.timeout.ms", "30000");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		consumer = new KafkaConsumer<String, String>(props);
+
+		try {
+			// Kafka Consumer subscribes list of topics here.
+			consumer.subscribe(topics);
+
+			// print the topic name
+			LOG.info("Subscribed to topics {}", String.join(",", topics));
+
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(100);
+				for (ConsumerRecord<String, String> record : records){
+					LOG.info("{} \t topic = {}, offset = {}, key = {}", dateFormat.format(new Date(record.timestamp())), record.topic(), record.offset(), record.key());
+					LOG.info("message value = {}", record.value());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			consumer.close();
+		}
+	}
 }
--- src/main/java/com/guilherme/kafka_samples/SimpleKafkaProducer.java
+++ src/main/java/com/guilherme/kafka_samples/SimpleKafkaProducer.java
@@ -5,58 +5,71 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Hello world!
+ * 
+ * @author ghott
  *
  */
-public class SimpleKafkaProducer 
-{
-    public static void main( String[] args )
-    {
-    	// Check arguments length value
-//        if(args.length == 0){
-//           System.out.println("Enter topic name");
-//           return;
-//        }
-        
-        //Assign topicName to string variable
-        String topicName = "TutorialClout"; //args[0].toString();
-        
-        // create instance for properties to access producer configs   
-        Properties props = new Properties();
-        
-        //Assign localhost id
-        props.put("bootstrap.servers", "localhost:9092");
-        
-        //Set acknowledgements for producer requests.      
-        props.put("acks", "all");
-        
-        //If the request fails, the producer can automatically retry,
-        props.put("retries", 0);
-        
-        //Specify buffer size in config
-        props.put("batch.size", 16384);
-        
-        //Reduce the no of requests less than 0   
-        props.put("linger.ms", 1);
-        
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.   
-        props.put("buffer.memory", 33554432);
-        
-        props.put("key.serializer", 
-           "org.apache.kafka.common.serialization.StringSerializer");
-           
-        props.put("value.serializer", 
-           "org.apache.kafka.common.serialization.StringSerializer");
-        
-        Producer<String, String> producer = new KafkaProducer
-           <String, String>(props);
-              
-        for(int i = 0; i < 10; i++)
-           producer.send(new ProducerRecord<String, String>(topicName, 
-              Integer.toString(i), Integer.toString(i) + " Java Producer Test"));
-                 System.out.println("Message sent successfully");
-                 producer.close();
-     }
+public class SimpleKafkaProducer {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SimpleKafkaProducer.class);
+	
+	public static void main(String[] args) {
+		
+		String origin = "origin.";
+
+		// Assign topicName to string variable
+		String topicName = origin+"guilhermehott"; // args[0].toString();
+
+		String message = "{"
+				+ "\"accessToken\":\"access-sandbox-d6fd887f-cdab-4398-9dca-ec4653e1fb5f\","
+				+ "\"startDate\":\"2017-03-01\","
+				+ "\"endDate\":\"2017-04-01\""
+				+ "}";
+
+		// create instance for properties to access producer configs
+		Properties props = new Properties();
+
+		// Assign localhost id
+		props.put("bootstrap.servers", "dev-kafka.clout.com:9092");
+
+		// Set acknowledgements for producer requests.
+		props.put("acks", "all");
+
+		// If the request fails, the producer can automatically retry,
+		props.put("retries", 0);
+
+		// Specify buffer size in config
+		props.put("batch.size", 16384);
+
+		// Reduce the no of requests less than 0
+		props.put("linger.ms", 1);
+
+		// The buffer.memory controls the total amount of memory available to
+		// the producer for buffering.
+		props.put("buffer.memory", 33554432);
+
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		Producer<String, String> producer = null;
+
+		try {
+			producer = new KafkaProducer<String, String>(props);
+			
+			for (int i = 0; i < 2; i++) {
+				producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(i), message));
+			}
+			
+			LOG.info("Message sent successfully to topic: {}", topicName);
+			LOG.info("message: {}", message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			producer.close();
+		}
+	}
 }
